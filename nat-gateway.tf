658d6345612736de28d483f6671b28f5b1ed01ad
Joseph McKnight Gunvalson <josephgunvalson@gmail.com>
2022-04-07 09:27:37 -0500

    initial commit

---
+++ nat-gateway.tf
@@ -0,0 +1,27 @@
+resource "aws_eip" "elastic-ip" {
+  vpc = true
+  associate_with_private_ip = var.eip_private_ip
+}
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.elastic-ip.allocation_id
+  subnet_id     = var.public_subnet_id
+
+  tags = {
+    Name = "${var.name}-${var.aws_subnet_id}"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  # depends_on = [aws_internet_gateway.example]
+}
+
+data "aws_route_table" "private-subnet-route-table" {
+  subnet_id = var.private_subnet_id
+}
+
+resource "aws_route" "route" {
+  route_table_id            = data.aws_route_table.private-subnet-route-table.id
+  nat_gateway_id =          aws_nat_gateway.nat-gateway.id
+  destination_cidr_block    = "0.0.0.0/0"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "eip_private_ip" {
+  type = string
+  default = null
+}
+
+variable "public_subnet_id" {
+  type = string
+}
+
+variable "private_subnet_id" {
+  type = string
+}
